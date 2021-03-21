26fa7482cc870aebb77313c557fd31e7b1c5cc6f
Bailey Dalton <baidalton@gmail.com>
2021-03-20 22:59:41 -0600

    7 day working

--- src/App.js
+++ src/App.js
@@ -18,7 +18,6 @@ function App() {
     e.preventDefault();
     fetch(
       `https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&units=${unit}&appid=669f8245a6857a3f843a044c9ce99772`
-      // `https://api.openweathermap.org/data/2.5/weather?zip=84042,us&units=imperial&appid=${process.env.REACT_APP_API_KEY}`
     )
       .then((res) => res.json())
       .then(
@@ -26,29 +25,29 @@ function App() {
           setIsLoaded(true);
           setCurrentWeather(result);
           console.log(result);
+                  fetch(
+          `https://api.openweathermap.org/data/2.5/onecall?lat=${result.coord.lat}&lon=${result.coord.lon}&units=${unit}&appid=669f8245a6857a3f843a044c9ce99772`
+        )
+          .then((res) => res.json())
+          .then(
+            (result) => {
+              setIsLoaded(true);
+              setDailyWeather(result.daily);
+              setHourlyWeather(result.hourly.splice(0, 12));
+              console.log(result);
+            },
+            (error) => {
+              setIsLoaded(true);
+              setError(error);
+            }
+          );
         },
         (error) => {
           setIsLoaded(true);
           setError(error);
         }
-      );
-    fetch(
-      // `https://api.openweathermap.org/data/2.5/onecall?lat=${currentWeather.coord.lat}&lon=${currentWeather.coord.lon}&units=${unit}&appid=${process.env.REACT_APP_API_KEY}`
-      `https://api.openweathermap.org/data/2.5/onecall?lat=40.2607&lon=-111.6549&units=${unit}&appid=669f8245a6857a3f843a044c9ce99772`
-    )
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setDailyWeather(result.daily);
-          setHourlyWeather(result.hourly.splice(0,12));
-          console.log(result);
-        },
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      );
+      )
+
   }
 
   // let date = new Date();
@@ -61,37 +60,37 @@ function App() {
 
       <form onSubmit={submit}>
         <div className="tempChoices">
-        <label>
-          <input
-            className="tempChoice"
-            type="radio"
-            name="units"
-            checked={unit === "imperial"}
-            value="imperial"
-            onChange={(e) => setUnit(e.target.value)}
-          />
-          Fahrenheit
-        </label>
-        <label>
-          <input
-            className="tempChoice"
-            type="radio"
-            name="units"
-            checked={unit === "metric"}
-            value="metric"
-            onChange={(e) => setUnit(e.target.value)}
-          />
-          Celcius
-        </label>
+          <label>
+            <input
+              className="tempChoice"
+              type="radio"
+              name="units"
+              checked={unit === "imperial"}
+              value="imperial"
+              onChange={(e) => setUnit(e.target.value)}
+            />
+            Fahrenheit
+          </label>
+          <label>
+            <input
+              className="tempChoice"
+              type="radio"
+              name="units"
+              checked={unit === "metric"}
+              value="metric"
+              onChange={(e) => setUnit(e.target.value)}
+            />
+            Celcius
+          </label>
         </div>
         <div className="zipcode">
-        <input
-          onChange={(e) => setZipCode(e.target.value)}
-          type="text"
-          name="zipcode"
-          placeholder="Zipcode"
-        />
-        <input type="submit" value="Search" />
+          <input
+            onChange={(e) => setZipCode(e.target.value)}
+            type="text"
+            name="zipcode"
+            placeholder="Zipcode"
+          />
+          <input type="submit" value="Search" />
         </div>
       </form>
 
@@ -102,36 +101,36 @@ function App() {
         </p>
       </div>
       <div>
-      <h2>12 Hour Forecast</h2>
-      <div className="hourlyWeather">
-        {hourlyWeather.map((hour) => {
-          return (
-            <div key={hour.dt}>
-              <p>{Math.round(hour.temp)}&#176;</p>
-            </div>
-          );
-        })}
-      </div>
+        <h2>12 Hour Forecast</h2>
+        <div className="hourlyWeather">
+          {hourlyWeather.map((hour) => {
+            return (
+              <div key={hour.dt}>
+                <p>{Math.round(hour.temp)}&#176;</p>
+              </div>
+            );
+          })}
+        </div>
       </div>
       <div>
-      <h2>7 Day Forecast</h2>
-      <div className="seven-day">
-        {dailyWeather.map((day) => {
-          return (
-            <div key={day.dt} className="daily-forecast">
-              {/* <h3>
+        <h2>7 Day Forecast</h2>
+        <div className="seven-day">
+          {dailyWeather.map((day) => {
+            return (
+              <div key={day.dt} className="daily-forecast">
+                {/* <h3>
                 <Moment format="MM/DD/YYYY">
                   {date.setDate(date.getDate() + 1)}
                 </Moment>
               </h3> */}
-              <p>{day.weather[0].description}</p>
-              <div className="temps">
-                <p className="high">{Math.round(day.temp.max)}&#176;</p>
-                <p className="low">{Math.round(day.temp.min)}&#176;</p>
+                <p>{day.weather[0].description}</p>
+                <div className="temps">
+                  <p className="high">{Math.round(day.temp.max)}&#176;</p>
+                  <p className="low">{Math.round(day.temp.min)}&#176;</p>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
         </div>
       </div>
     </div>
